c3fb63b7dc87f94623ef5646deacf84196f20d34
Rohan Roy Tharakan <rohan.tharakan@gmail.com>
2020-03-20 10:52:14 +0800

    Fixing

--- lib/views/Nearby.dart
+++ lib/views/Nearby.dart
@@ -4,9 +4,27 @@ import 'package:flutter/material.dart';
 
 class Nearby extends StatelessWidget{
    const Nearby({Key key}) : super(key: key);
+<<<<<<< HEAD
+=======
+   @override
+  _MyAppState createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<Nearby> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(1.290270, 	103.851959);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+>>>>>>> parent of cd69335... with map-2
   @override
   Widget build(BuildContext cntxt){
     return Scaffold(
+<<<<<<< HEAD
 
       body: Center(
           child: Text('Nearby Locations Page'),
@@ -14,4 +32,15 @@ class Nearby extends StatelessWidget{
     );
   }
 
+=======
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      );
+  }
+>>>>>>> parent of cd69335... with map-2
 }
