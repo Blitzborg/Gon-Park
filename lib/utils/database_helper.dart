4401704c5fd2603daff640d867152e0b228d8c5d
Zou Yulin <zouy0013@e.ntu.edu.sg>
2020-03-27 17:05:18 +0800

    backend

---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:parkapp/models/bookmark.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper _databaseHelper;
+  static Database _database;
+
+  String bookmarkTable = 'bookmarkTable';
+  String colName = 'LocationName';
+  String colID = 'LocationID';
+  String colX = 'cX';
+  String colY = 'cY';
+
+  DatabaseHelper._createInstance();
+
+  factory DatabaseHelper() {
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper
+          ._createInstance();
+    }
+    return _databaseHelper;
+  }
+
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initializeDatabase();
+    }
+    return _database;
+  }
+
+  Future<Database> initializeDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'bookmarks.db';
+
+    // Open/create the database at a given path
+    var bookmarksDatabase =
+        await openDatabase(path, version: 1, onCreate: _createDb);
+    return bookmarksDatabase;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        'CREATE TABLE $bookmarkTable($colID TEXT PRIMARY KEY,'
+            '$colName TEXT, $colX DOUBLE, $colY DOUBLE)');
+  }
+
+  Future<List<Map<String, dynamic>>> getBookmarkMapList() async {
+    Database db = await this.database;
+
+    var result = await db.query(bookmarkTable, orderBy: '$colName ASC');
+    return result;
+  }
+
+  Future<int> insertBookmark(Bookmark bookmark) async {
+    Database db = await this.database;
+    var result = await db.insert(bookmarkTable, bookmark.toMap());
+    return result;
+  }
+
+  Future<int> updateBookmark(Bookmark bookmark) async {
+    var db = await this.database;
+    var result = await db.update(bookmarkTable, bookmark.toMap(),
+        where: '$colID = ?', whereArgs: [bookmark.LocationID]);
+    return result;
+  }
+
+  Future<int> deleteBookmark(String ID) async {
+    var db = await this.database;
+    int result =
+        await db.rawDelete("DELETE FROM $bookmarkTable WHERE LocationID = '$ID'");
+    return result;
+  }
+
+  Future<int> getCount() async {
+    Database db = await this.database;
+    List<Map<String, dynamic>> x =
+        await db.rawQuery('SELECT COUNT (*) from $bookmarkTable');
+    int result = Sqflite.firstIntValue(x);
+    return result;
+  }
+
+  Future<List<Bookmark>> getBookmarkList() async {
+    var bookmarkMapList = await getBookmarkMapList();
+    int count =
+        bookmarkMapList.length;
+
+    List<Bookmark> noteList = List<Bookmark>();
+    // For loop to create a 'Note List' from a 'Map List'
+    for (int i = 0; i < count; i++) {
+      noteList.add(Bookmark.fromMapObject(bookmarkMapList[i]));
+    }
+
+    return noteList;
+  }
+}
--- lib/views/Bookmarks.dart
+++ lib/views/Bookmarks.dart
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
+=======
+import 'dart:async';
+>>>>>>> Stashed changes
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:parkapp/models/bookmark.dart';
+import 'package:parkapp/utils/database_helper.dart';
 
+<<<<<<< Updated upstream
 class Bookmarks extends StatelessWidget{
   const Bookmarks({Key key}) : super(key: key);
 
@@ -48,3 +56,104 @@ Widget _myListView(BuildContext context) {
     },
   );
 }
+=======
+class Bookmarks extends StatefulWidget {
+  const Bookmarks({Key key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return BookmarkListState();
+  }
+}
+
+class BookmarkListState extends State<Bookmarks> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Bookmark> bookmarkList;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (bookmarkList == null) {
+      bookmarkList = List<Bookmark>();
+      updateListView();
+    }
+
+    return Scaffold(
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB clicked');
+          insertInit();
+          updateListView();
+        },
+        tooltip: 'Add Note',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getNoteListView() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text(
+              this.bookmarkList[position].LocationName,
+              style: titleStyle,
+            ),
+            trailing: GestureDetector(
+              child: Icon(
+                Icons.delete,
+                color: Colors.grey,
+              ),
+              onTap: () {
+                _delete(context, bookmarkList[position]);
+              },
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  void _delete(BuildContext context, Bookmark bookmark) async {
+    int result = await databaseHelper.deleteBookmark(bookmark.LocationID);
+    if (result != 0) {
+      _showSnackBar(context, 'Note Deleted Successfully');
+      updateListView();
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(content: Text(message));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  void updateListView() {
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    dbFuture.then((database) {
+      Future<List<Bookmark>> noteListFuture = databaseHelper.getBookmarkList();
+      noteListFuture.then((bookmarkList) {
+        setState(() {
+          this.bookmarkList = bookmarkList;
+          this.count = bookmarkList.length;
+        });
+      });
+    });
+  }
+
+  void insertInit() async {
+    Bookmark initBookmark =
+        new Bookmark("BLK 37 CIRCUIT ROAD", "M16	", 33937.7242, 34516.7499);
+    this.databaseHelper.insertBookmark(initBookmark);
+  }
+}
+>>>>>>> Stashed changes
