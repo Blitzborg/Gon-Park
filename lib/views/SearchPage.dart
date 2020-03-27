99c259c6bee3da0e0ea282631555c5ebc8b669b8
Lat Han <62240778+thinlathan@users.noreply.github.com>
2020-03-27 19:07:40 +0800

    Update SearchPage.dart

--- lib/views/SearchPage.dart
+++ lib/views/SearchPage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class SearchPage extends StatelessWidget{
   const SearchPage({Key key}) : super(key: key);
+
+  Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
   @override
   Widget build(BuildContext cntxt) {
