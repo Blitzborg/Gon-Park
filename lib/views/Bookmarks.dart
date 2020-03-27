1a7672a9c978dab8b1c26b1375ca65fe1c17cc9a
Zou Yulin <zouy0013@e.ntu.edu.sg>
2020-03-27 17:07:02 +0800

    backend

--- lib/views/Bookmarks.dart
+++ lib/views/Bookmarks.dart
@@ -1,62 +1,11 @@
-<<<<<<< Updated upstream
-=======
 import 'dart:async';
->>>>>>> Stashed changes
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:parkapp/models/bookmark.dart';
 import 'package:parkapp/utils/database_helper.dart';
 
-<<<<<<< Updated upstream
-class Bookmarks extends StatelessWidget{
-  const Bookmarks({Key key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext cntxt) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Bookmarked Places')),
-      body: BodyLayout(),
-    );
-  }
-}
-
-class BodyLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return _myListView(context);
-  }
-}
-
-Widget _myListView(BuildContext context) {
-  final titles = ['Place 1', 'Place 2', 'Place 3', 'Place 4', 'Place 5'];
-
-  final icons = [
-    Icons.location_on,
-    Icons.location_on,
-    Icons.location_on,
-    Icons.location_on,
-    Icons.location_on
-  ];
-
-  return ListView.builder(
-    itemCount: titles.length,
-    itemBuilder: (context, index) {
-      return Card(
-        //                           <-- Card widget
-        child: ListTile(
-          onTap: () {
-            print('tapped');
-          },
-          leading: Icon(icons[index]),
-          title: Text(titles[index]),
-          trailing: Icon(Icons.keyboard_arrow_right),
-        ),
-      );
-    },
-  );
-}
-=======
 class Bookmarks extends StatefulWidget {
   const Bookmarks({Key key}) : super(key: key);
   @override
@@ -156,4 +105,3 @@ class BookmarkListState extends State<Bookmarks> {
     this.databaseHelper.insertBookmark(initBookmark);
   }
 }
->>>>>>> Stashed changes
