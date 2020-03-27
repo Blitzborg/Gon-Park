026a85e1f495a8e385b7210f7c214860046cdc9f
Lat Han <62240778+thinlathan@users.noreply.github.com>
2020-03-27 19:07:37 +0800

    Update Nearby.dart

--- lib/views/Nearby.dart
+++ lib/views/Nearby.dart
@@ -1,14 +1,13 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Nearby extends StatefulWidget{
    const Nearby({Key key}) : super(key: key);
    @override
   _MyAppState createState() => _MyAppState();
-
 }
 
 
@@ -16,7 +15,10 @@ class _MyAppState extends State<Nearby> {
   Completer<GoogleMapController> _controller = Completer();
   static const LatLng _center = const LatLng(1.290270, 	103.851959);
 
-
+  void getLocation()async{
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
