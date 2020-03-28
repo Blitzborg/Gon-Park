e7cd3733a377721b30a991cd01a8d80b31059988
Zou Yulin <zouy0013@e.ntu.edu.sg>
2020-03-29 04:27:34 +0800

    ios fixed, camera locate current location done

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -21,15 +21,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 1.34, longitude: 103.68, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         self.view.addSubview(mapView)
 
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 1.34, longitude: 103.68)
+        marker.title = "Singapore"
+        marker.snippet = "Singapore"
         marker.map = mapView
   }
 }
--- lib/views/Nearby.dart
+++ lib/views/Nearby.dart
@@ -5,22 +5,25 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Nearby extends StatefulWidget {
-
   @override
-  _MyAppState createState() => _MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
 final Map<String, Marker> _markers = {};
 
-class _MyAppState extends State<Nearby> {
-  Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(1.290270, 103.851959);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
+class MyAppState extends State<Nearby> {
+  GoogleMapController mapController;
+  Position userLocation;
+  Geolocator geolocator = Geolocator();
 
   @override
+  void initState() {
+    super.initState();
+    _getLocation().then((position) {
+      userLocation = position;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -36,16 +39,24 @@ class _MyAppState extends State<Nearby> {
         ],
       ),
       body: GoogleMap(
-        onMapCreated: _onMapCreated,
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
         initialCameraPosition: CameraPosition(
-          target: _center,
+          target: LatLng(1.3521, 103.8198),
           zoom: 11.0,
         ),
-        markers: _markers.values.toSet(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _getLocation();
+          userLocation == null
+              ? CircularProgressIndicator()
+              : mapController.animateCamera(CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                      target:
+                          LatLng(userLocation.latitude, userLocation.longitude),
+                      zoom: 15.0),
+                ));
         },
         tooltip: 'Get Location',
         child: Icon(Icons.location_searching),
@@ -53,19 +64,14 @@ class _MyAppState extends State<Nearby> {
     );
   }
 
-  void _getLocation() async {
-    var currentLocation = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-    print(currentLocation.toString());
-
-    setState(() {
-      _markers.clear();
-      final marker = Marker(
-        markerId: MarkerId("curr_loc"),
-        position: LatLng(currentLocation.latitude, currentLocation.longitude),
-        infoWindow: InfoWindow(title: 'Your Location'),
-      );
-      _markers["Current Location"] = marker;
-    });
+  Future<Position> _getLocation() async {
+    var currentLocation;
+    try {
+      currentLocation = await geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
   }
 }
