19456b24085527203171c522ac852829ca2c1f1c
Lat Han <62240778+thinlathan@users.noreply.github.com>
2020-03-30 23:12:59 +0800

    Update Nearby.dart

--- lib/views/Nearby.dart
+++ lib/views/Nearby.dart
@@ -1,84 +1,3 @@
-<<<<<<< HEAD
-import 'dart:async';
-=======
-<<<<<<< Updated upstream
-
->>>>>>> origin/jaswanth
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-class Nearby extends StatefulWidget {
-  const Nearby({Key key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-final Map<String, Marker> _markers = {};
-
-class _MyAppState extends State<Nearby> {
-  Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(1.290270, 103.851959);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: () {
-              print("Search");
-            },
-          )
-        ],
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
-        markers: _markers.values.toSet(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _getLocation();
-        },
-        tooltip: 'Get Location',
-        child: Icon(Icons.location_searching),
-      ),
-    );
-  }
-
-<<<<<<< HEAD
-  void _getLocation() async {
-    var currentLocation = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-    print(currentLocation.toString());
-
-    setState(() {
-      _markers.clear();
-      final marker = Marker(
-        markerId: MarkerId("curr_loc"),
-        position: LatLng(currentLocation.latitude, currentLocation.longitude),
-        infoWindow: InfoWindow(title: 'Your Location'),
-      );
-      _markers["Current Location"] = marker;
-    });
-  }
-}
-=======
-}
-=======
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -86,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:io';
-import '../model/car_park.dart' ;
+import './model/car_park.dart' ;
 import 'dart:convert';
 import 'package:csv/csv.dart';
 import 'package:flutter/services.dart' show rootBundle;
@@ -226,5 +145,4 @@ class _MyAppState extends State<Nearby> {
   }
 }
 // markers: allMarkers.values.toSet()
->>>>>>> Stashed changes
 >>>>>>> origin/jaswanth
