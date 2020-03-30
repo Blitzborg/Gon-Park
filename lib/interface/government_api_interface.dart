d1f80c068e2b4e27c9e9c9834265c5583ac0aace
Lat Han <62240778+thinlathan@users.noreply.github.com>
2020-03-30 23:12:56 +0800

    Update government_api_interface.dart

--- lib/interface/government_api_interface.dart
+++ lib/interface/government_api_interface.dart
@@ -1,23 +1,14 @@
-
 import 'package:http/http.dart' as http;
 import 'dart:convert' show json;
 import 'dart:async';
 
-class GovtDataAPI
-{
-  Future<String> getData() async
-  {
-    var response = await http.get
-      (
-        Uri.encodeFull("https://api.data.gov.sg/v1/transport/carpark-availability"),
-        headers:
-        {
-          "Accept": "application/json"
-        }
-    );
+class GovtDataAPI {
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull(
+            "https://api.data.gov.sg/v1/transport/carpark-availability"),
+        headers: {"Accept": "application/json"});
 
     return response.body;
   }
 }
-
-
